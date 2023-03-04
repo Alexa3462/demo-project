4db866c29ebb21f44e5b4e6fff3ca8a8c7eac103
alevgn <alevgun85@gmail.com>
2023-03-04 14:54:26 -0500

    Selenium Grid updated ip

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.201.8.15";
+                        String gridAddress = "44.200.160.139";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
